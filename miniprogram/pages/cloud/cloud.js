3ad9da94d619798ed3d696c77b2f8ab715e651da
insanejohnny <41359461+insanejohnny@users.noreply.github.com>
2020-06-09 16:44:24 +0800

    Add files via upload
    云数据库学习

--- miniprogram/pages/cloud/cloud.js
+++ miniprogram/pages/cloud/cloud.js
@@ -1,4 +1,5 @@
 // miniprogram/pages/cloud/cloud.js
+const db = wx.cloud.database();
 Page({
 
   /**
@@ -6,6 +7,64 @@ Page({
    */
   data: {
 
+  },
+
+  insert: function(){
+    // db.collection('user').add({
+    //   data:{
+    //     name: 'jerry',
+    //     age: 20
+    //   },
+    //   success: res => {
+    //     console.log(res)
+    //   },
+    //   fail: err => {
+    //     console.log()
+    //   }
+    // })
+    db.collection('user').add({
+      data:{
+        name: "jack",
+        age : 18
+      }
+    }).then(res=>{
+      console.log(res);
+    }).catch(res=>{
+      console.log(err);
+    })
+  },
+
+  update: function(){
+    db.collection('user').doc('5a93cec95edf473100694d8772536556').update({
+      data: {
+        age: 21
+      }
+    }).then(res=>{
+      console.log(res);
+    }).catch(res=>{
+      console.log(err);
+    })
+  },
+
+  search: function(){
+    db.collection('user').where({
+      name: 'jerry'
+    }).get().then(res=>{
+      console.log(res);
+    }).catch(res=>{
+      console.log(err);
+    })
+  },
+
+  delete: function(){
+    db.collection('user')
+    .doc('5a93cec95edf473100694d8772536556')
+    .remove()
+    .then(res=>{
+      console.log(res);
+    }).catch(res=>{
+      console.log(err);
+    })
   },
 
   /**
